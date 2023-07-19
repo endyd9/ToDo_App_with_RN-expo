7d67a90110bf77e66ee1dd4d4fcdc352de65f6f3
endyd9 <endyd9@gmail.com>
2023-07-20 03:24:44 +0900

    헤더, 인풋

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TextInput,
+} from "react-native";
+import { theme } from "./color";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") return;
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.gray }}
+          >
+            ToDo
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.gray : "white" }}
+          >
+            Wish
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          returnKeyType="done"
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          placeholder={working ? "Add To Do" : "Where Are You Going?"}
+          style={styles.input}
+          value={text}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +63,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 25,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 70,
+    marginBottom: 20,
+  },
+  btnText: {
+    fontSize: 35,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 30,
+    fontSize: 20,
   },
 });
